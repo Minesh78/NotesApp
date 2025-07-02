35bd30bfc97719656460c021af93c0c7b04087eb
Minesh Rajput <110811001+Minesh78@users.noreply.github.com>
2025-07-03 04:29:30 +0530

    Updated base_url and added Vercel.json

--- frontend/notes-app/src/utils/axiosInstance.js
+++ frontend/notes-app/src/utils/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-import { BASE_URL } from "./constants";
+// import { BASE_URL } from "./constants";
 
 const axiosInstance = axios.create({
-	baseURL: BASE_URL,
+	baseURL: `${process.env.REACT_APP_BASE_URL}`,
 	timeout: 1000,
 	headers: {
 		"Content-Type": "application/json",
