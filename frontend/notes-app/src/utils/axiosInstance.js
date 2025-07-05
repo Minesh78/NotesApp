69f38d0aea938ea599631f2d743cb393ce97d3a5
Minesh Rajput <110811001+Minesh78@users.noreply.github.com>
2025-07-05 23:27:02 +0530

    fixed axios baseURL env config

--- frontend/notes-app/src/utils/axiosInstance.js
+++ frontend/notes-app/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 // import { BASE_URL } from "./constants";
 
 const axiosInstance = axios.create({
-	baseURL: `${process.env.REACT_APP_BASE_URL}`,
+	baseURL:import.meta.env.VITE_API_BASE_URL,
 	timeout: 1000,
 	headers: {
 		"Content-Type": "application/json",
